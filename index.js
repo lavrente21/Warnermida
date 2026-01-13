f72df759eec0b4741adf9ba33478dff041a5e308
Lavrente Mambondo <lavrentemambondo@gmail.com>
2026-01-13 17:58:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-    origin: 'https://warnermedia.netlify.app'
+    origin: 'https://warnermidia.netlify.app'
 }));app.use(express.json());
 
 const pool = new Pool({
